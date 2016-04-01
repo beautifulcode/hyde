7b5279f3567dd034240cc9ea7625470ec52036d3
Aaron Glenn <ag@appcolony.ca>
2016-04-01 11:56:07 -0700

    Fix mix package details

--- mix.exs
+++ mix.exs
@@ -5,37 +5,35 @@ defmodule Hyde.Mixfile do
     [app: :hyde,
      version: "0.0.1",
      elixir: "~> 1.1",
-     description: "Feature Toggles for Elixir - Basic Redis backed feature
-     toggles for individual users or named groups",
-     maintainers: "Aaron Glenn",
-     licences: "MIT",
-     links: %{github: "https://github.com/beautifulcode/hyde"},
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
     [
       {:exredis, "~> 0.2"},
       {:earmark, "~> 0.2", only: :dev},
       {:ex_doc, "~> 0.11", only: :dev}
+    ]
+  end
+
+  defp description do
+    """
+    Feature Toggles for Elixir - Basic Redis backed feature toggles for individual users or named groups
+    """
+  end
+
+  defp package do
+    [
+     maintainers: "Aaron Glenn",
+     licences: "MIT",
+     links: %{"Github" => "https://github.com/beautifulcode/hyde"}
     ]
   end
 end
